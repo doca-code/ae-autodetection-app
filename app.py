66ccdb0bd7b1840108ed3e9bca7d0f787e990497
Wanderson Andrade <wanderson.andrade@gmail.com>
2025-03-23 15:51:49 -0300

    data_ingestion feature

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import boto3
 
 from stack.s3_stack import S3Stack
 from stack.ae_stack import AeStack 
+from stack.dev_instance_stack import DevInstanceStack
 
 region_name = boto3.session.Session().region_name
 env={'region': region_name}
@@ -11,7 +12,8 @@ env={'region': region_name}
 app = cdk.App()
 #vpc_stack = AiVpcNetworkStack(app, "VPCStack", env=env)
 
-ae_bucket = S3Stack(app, "ae-bucket-east-1", env=env) #self, stack name, env=region
-AeStack(app, "ae-east-1", bucket=ae_bucket.bucket, env=env)
+#ae_bucket = S3Stack(app, "ae-bucket-east-1", env=env) #self, stack name, env=region
+#AeStack(app, "ae-east-1", bucket=ae_bucket.bucket, env=env)
+DevInstanceStack(app, "dev-instance-stack-east-1", env=env)
 
 app.synth()
---
+++ stack/dev_instance_stack.py
@@ -0,0 +1,50 @@
+from constructs import Construct
+from aws_cdk import (
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    Stack,
+    RemovalPolicy
+)
+
+class DevInstanceStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        # Create a VPC
+        new_vpc = ec2.Vpc(self, "VPC")
+
+        # IAM Role for EC2 (Allows SSM & Basic Access)
+        role = iam.Role(
+            self, "EC2Role",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore")
+            ]
+        )
+        role.apply_removal_policy(RemovalPolicy.DESTROY)
+
+        # Create the EC2 instance
+        instance = ec2.Instance(
+            self, "DevInstance",
+            instance_type=ec2.InstanceType("t3.large"),
+            machine_image=ec2.MachineImage.latest_amazon_linux(),
+            vpc=new_vpc,
+            role=role,
+            removal_policy=RemovalPolicy.DESTROY
+        )
+
+        # User Data to set up the instance
+        instance.user_data.add_commands(
+            "yum update -y",
+            "yum install -y unzip git python3-pip",  # Install dependencies (git, unzip, pip)
+            "cd /home/ec2-user",
+            "git clone https://github.com/doca-code/ae-autodetection-app.git",  # Clone the full repo
+            "cd ae-autodetection-app/data_ingestion",
+            "unzip data_ingestion.zip -d ../twitter-scripts",  # Extract ZIP into twitter-scripts folder
+            "rm data_ingestion.zip",  # Clean up ZIP file
+            "pip3 install -r requirements.txt",  # Install Python dependencies
+            "python3 -m spacy download en_core_web_sm",  # Download spaCy model
+            "python3 stream.py"  # Start the Twitter API stream listener
+        )
+        # Make sure VPC is deleted when the stack is destroyed
+        new_vpc.apply_removal_policy(RemovalPolicy.DESTROY)
